c14626cb680ba8bbc3ab39e2e4dcb77cb3f29286
HDccer <denghuanccer@gmail.com>
2022-09-26 18:08:45 -0400

    install packages

---
+++ Main.R
@@ -0,0 +1,9 @@
+# install packages
+
+install.packages(c(
+  "colorBlindness", "directlabels", "dplyr", "gameofthrones", "ggforce", 
+  "gghighlight", "ggnewscale", "ggplot2", "ggraph", "ggrepel", 
+  "ggtext", "ggthemes", "hexbin", "Hmisc", "mapproj", "maps", 
+  "munsell", "ozmaps", "paletteer", "patchwork", "rmapshaper", "scico", 
+  "seriation", "sf", "stars", "tidygraph", "tidyr", "wesanderson" 
+))
